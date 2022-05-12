00078b1808411fc2880632fbb8179782f82c3c5f
Anastasiya4567 <302439@pw.edu.pl>
2022-05-12 10:28:56 +0200

    New empty document on backend can be added

--- src/main/java/com/thesis/thesis/application/DocumentFacade.java
+++ src/main/java/com/thesis/thesis/application/DocumentFacade.java
@@ -3,6 +3,8 @@ package com.thesis.thesis.application;
 import com.thesis.thesis.infractructure.port.DocumentPort;
 import org.springframework.data.domain.Page;
 
+import java.util.UUID;
+
 public class DocumentFacade {
 
     private final DocumentPort documentPort;
@@ -15,7 +17,8 @@ public class DocumentFacade {
         return documentPort.getAllDocuments(pageIndex, pageSize);
     }
 
-    public void addNewDocument(PDFDocumentDTO pdfDocumentDTO) {
+    public void addNewDocument(String title) {
+        PDFDocumentDTO pdfDocumentDTO = new PDFDocumentDTO(UUID.randomUUID().toString(), title);
         documentPort.addNewDocument(pdfDocumentDTO);
     }
 }
